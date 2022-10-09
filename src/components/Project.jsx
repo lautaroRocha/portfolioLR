1c8f622b40631b0eecb31d79003994c79c99936f
Lautaro Rocha <lautarovrocha@gmail.com>
2022-10-09 20:29:34 -0300

    css improvements + back button added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React, {useContext} from "react";
+import { Link } from "react-router-dom";
 import {TextContext} from '../App.js'
 import '../styles/project.css';
 
@@ -15,7 +16,6 @@ function Project(){
        sites = Object.values(works);
        pros = Object.values(sites[2]);
        project = pros.find(site => site.title === title);
-       console.log(project.techs)
     }
 
 
@@ -24,15 +24,20 @@ function Project(){
         <>
         <div className="container">
         { text &&
-<>
+        <>
             <h2 className="project project-title">{project.title}</h2>
             <div className="project-links">
                 <a target="blank" href={project.link}>DEMO</a>
                 <a target="blank" href={project.repo}>CODE</a>
             </div>
+            <div className="project-window">
             <p className="project project-brief">{project.brief}</p>
             <img src={project.snapshot} alt="Picture of the website" className="project-snap"/> 
-            </>  
+            </div>
+            <Link to="/projects">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.427 3.021h-7.427v-3.021l-6 5.39 6 5.61v-3h7.427c3.071 0 5.561 2.356 5.561 5.427 0 3.071-2.489 5.573-5.561 5.573h-7.427v5h7.427c5.84 0 10.573-4.734 10.573-10.573s-4.733-10.406-10.573-10.406z"/></svg>
+             </Link>
+        </>  
         }   
 </div>
         
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,12 @@
-import React, {useContext} from "react";
+import React, {useContext, useRef} from "react";
+import { Link } from "react-router-dom";
 import FadeIn from "../utilities/FadeIn";
 import MinProj from "./MinProj";
 import {TextContext} from '../App';
 import '../styles/tools.css'
 
 function Projects(){
-
+    const divToScroll = useRef();
     let text = useContext(TextContext);
     let works, sites, pros;
 
@@ -14,7 +15,9 @@ function Projects(){
         sites = Object.values(works);
         pros = Object.values(sites[2]);
     }
-   
+    const scrollDiv = (e) => {
+        divToScroll.current.scrollLeft += e.deltaY + 1 ;
+        }
     return(
         <>
         <FadeIn>
@@ -25,16 +28,19 @@ function Projects(){
             <p>{text.projects.body}</p>
         </div>}
         {pros &&
-        <div className="tools-img">
-            {console.log(pros)}
+        <div className="tools-img text-gallery" onWheel={scrollDiv} ref={divToScroll}>
           {pros.map((site, idx) => {
             return(
                 <MinProj site={site} key={idx} />
                 )})
             }
         </div>}
+        <Link to="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.427 3.021h-7.427v-3.021l-6 5.39 6 5.61v-3h7.427c3.071 0 5.561 2.356 5.561 5.427 0 3.071-2.489 5.573-5.561 5.573h-7.427v5h7.427c5.84 0 10.573-4.734 10.573-10.573s-4.733-10.406-10.573-10.406z"/></svg>
+        </Link>
         </div>
         </FadeIn>
+        
         </>
     )
 
