bfb65fcd1b17d15920c917e90c93b559204f8243
Lautaro Rocha <lautarovrocha@gmail.com>
2022-11-02 10:57:26 -0300

    start to develop new componnent

---
+++ src/components/NameAndIntro.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Name from '../utilities/Name';
+
+function NameAndIntro(){
+    return(
+        <div className="scrolable-window">
+            <Name>
+            </Name>
+            <p className="scrollabe-window-text">
+                
+            </p>
+        </div>
+    )
+}
+
+export default NameAndIntro;
